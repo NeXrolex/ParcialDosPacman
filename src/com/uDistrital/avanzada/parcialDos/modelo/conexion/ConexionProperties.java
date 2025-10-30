9fa8cc2ecceed1a8dc8460aba514330a006c3c08
santi <santi@DESKTOP-LU7UNG7>
2025-10-30 15:05:41 -0500

    ConexionProperties

--- src/com/uDistrital/avanzada/parcialDos/modelo/conexion/ConexionProperties.java
+++ src/com/uDistrital/avanzada/parcialDos/modelo/conexion/ConexionProperties.java
@@ -5,18 +5,88 @@
 package com.uDistrital.avanzada.parcialDos.modelo.conexion;
 
 import com.uDistrital.avanzada.parcialDos.modelo.interfaces.IConexion;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 /**
- * 
+ * Conecta con el archivo de propiedades y cumple el contrato de servios
+ * de IConexion
+ *
  * @author Alex
  */
 public class ConexionProperties implements IConexion {
 
-    @Override
-    public Object conexion() {
-       
-        return null;
-       
+    private File archivo;
+
+    /**
+     * Constructor vacio
+     *
+     */
+    public ConexionProperties() {
     }
-    
+
+    /**
+     * Constructor que asigna el archivo
+     *
+     * @param archivo Archivo
+     */
+    public ConexionProperties(File archivo) {
+        this.archivo = archivo;
+    }
+
+    /**
+     * Metodo que conecta con el archivo de propiedades
+     *
+     * @return Properties
+     */
+    @Override
+    public Properties conexion() {
+        try {
+
+            if (archivo == null) {
+                throw new IllegalStateException("No se ha asociado un"
+                        + " archivo .properties.");
+            }
+            //Si se escoge un archivo incorrecto
+            if (!archivo.exists() || !archivo.isFile()) {
+
+                throw new IllegalStateException("El archivo .properties no "
+                        + "existe o no es válido: " + archivo);
+            }
+
+            Properties props = new Properties();
+            /* Usamos try para despues de su uso se cierre el archivo, 
+        importante para las buenas parcticas*/
+            try (FileInputStream aux = new FileInputStream(archivo)) {
+                props.load(aux);
+                return props;
+            }
+
+        } catch (IOException ioe) {
+
+        }
+
+        return null;
+
+    }
+    /**
+     * Obtiene un archivo
+     *
+     * @return Archivo
+     */
+    public File getArchivo() {
+        return archivo;
+    }
+
+    /**
+     * Asigna un archivo
+     *
+     * @param archivo Archivo
+     */
+    public void setArchivo(File archivo) {
+        this.archivo = archivo;
+    }
+
 }
