1f502161f1c7631ee6b15bb854b8bac58a4e01aa
Steven Londoño <stlondonoh@udistrital.edu.co>
2025-11-05 13:43:17 -0500

    placeholder movimiento (no tocar vista)

--- src/com/uDistrital/avanzada/parcialDos/vista/VentanaJuego.java
+++ src/com/uDistrital/avanzada/parcialDos/vista/VentanaJuego.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.uDistrital.avanzada.parcialDos.vista;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -10,13 +11,16 @@ import java.awt.*;
  *
  * @author Steven
  */
-public class VentanaJuego extends JFrame{
-    
-    public VentanaJuego(){
+public class VentanaJuego extends JFrame {
+
+    public VentanaJuego() {
         setTitle("Pac-Man");
         setSize(900, 700);
         setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ImageIcon imgPlaceholder = new ImageIcon(getClass().getResource("/Specs/ImagesAndGifs/pacman24px.png"));
+        JLabel placeholderLabel = new JLabel(imgPlaceholder);
+        add(placeholderLabel);
         setLocationRelativeTo(null);
         setVisible(true);
     }
